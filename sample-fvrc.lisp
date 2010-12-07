c232a98428dba131a7ca53727bca9e9a1c9de3ed
Antoni Grzymała <antekgrzymala@gmail.com>
2010-12-07 10:49:20 +0100

    Added sample .fvrc.lisp

---
+++ sample-fvrc.lisp
@@ -0,0 +1,8 @@
+(defparameter *company-data* (list
+			      :name "Nazwa firmy"
+			      :address "Ulica, nr"
+			      :postcode "Kod Pocztowy"
+			      :city "Miasto"
+			      :nip "NIP firmy"
+			      :email "firma@firma.tld"
+			      :account "00 1111 2222 3333 4444 5555 6666"))
