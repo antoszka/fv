28bf0b030812dc34635c9cc5ee66ef22c82f7ece
Antoni Grzymała <antekgrzymala@gmail.com>
2012-08-17 15:39:10 +0200

    Moved some stuff to sample-fvrc.lisp.

--- sample-fvrc.lisp
+++ sample-fvrc.lisp
@@ -2,11 +2,21 @@
 (in-package #:fv)
 ;;
 
-(defparameter *company-data* (list
-                              :name "Nazwa firmy"
-                              :address "Ulica, nr"
-                              :postcode "Kod Pocztowy"
-                              :city "Miasto"
-                              :nip "NIP firmy"
-                              :email "firma@firma.tld"
-                              :account "00 1111 2222 3333 4444 5555 6666"))
+(defparameter *company-data*
+  (list
+   :name "Nazwa firmy"
+   :address "Ulica, nr"
+   :postcode "Kod Pocztowy"
+   :city "Miasto"
+   :nip "NIP firmy"
+   :email "firma@firma.tld"
+   :account "00 1111 2222 3333 4444 5555 6666"))
+
+(defparameter *monthly-billed-clients*
+  (list 'foo 
+        'bar
+        'baz
+        'xyzzy))
+
+(defparameter *program-directory*
+  (make-pathname :directory '(:absolute "home" "user" "fv")))
